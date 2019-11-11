16e65452555344fd0dd9dae85fcdee3813f1c9d2
Lindsey Pett <31856814+lpett@users.noreply.github.com>
2019-11-11 14:48:43 -0500

    more

--- _js/scripts.js
+++ _js/scripts.js
@@ -242,7 +242,7 @@ $( "#contactForm" ).submit( function( e ) {
     $.ajax( {
 
         // Change the email address here:
-        url: "https://formspree.io/jan.czizikow@Fmail.com",
+        url: "Lindsey.Pett@uvm.edu",
         method: "POST",
         data: $( this ).serialize(),
         dataType: "json",
